459d9aae61e9884c3a2c574e74cc77f4b36a61f6
Dorian Goepp <dorian.goepp@gmail.com>
2015-08-19 17:41:25 +0200

    Make the simple walk program parameterisable

--- src/simple_walk.cpp
+++ src/simple_walk.cpp
@@ -15,13 +15,15 @@ int main(int argc, char **argv)
 
 	// pulsation of the walking movement
 	double pulsation;
-	n.param<double>("pulsation",pulsation,1.0);
+	n.param<double>("pulsation", pulsation, 1.0);
 	// Speed for the wheels
 	int wheel_speed;
-	n.param<int>("wheel_speed",wheel_speed,50);
+	n.param<int>("wheel_speed", wheel_speed, 50);
 
 	// amplitudes of the oscilators
-	int amp0 = 140, amp1=300, amp3=40;
+	int amp0, amp1;
+	n.param<int>("amplitude_0", amp0, 140);
+	n.param<int>("amplitude_1", amp1, 300);
 
 	ROS_INFO_STREAM("Pulsation is set to " << pulsation);
 	ROS_INFO_STREAM("Wheels speed is set to " << wheel_speed);
@@ -44,10 +46,22 @@ int main(int argc, char **argv)
 	ros::ServiceClient ids_client = n.serviceClient<dynamixel_control::GetIDs>("/dynamixel_control/getids");
 	dynamixel_control::GetIDs ids_srv;
 
+	// Get the list of joint mode and wheel mode actoators for this robot
+	std::vector<int> pos_ctrl_ids_int;
+	std::vector<int> speed_ctrl_ids_int;
+	n.getParam("actuator_ids/joint_mode", pos_ctrl_ids_int);
+	n.getParam("actuator_ids/wheelmode", speed_ctrl_ids_int);
+
+	// Get the configuration parameters for each actuator
+	std::vector<bool> invert_position;
+	n.getParam("invert_position", invert_position);
+	std::vector<int> associated_oscillator;
+	n.getParam("associated_oscillator", associated_oscillator);
+
 	// ids of actuators controlled in position (joint mode)
-	std::vector<unsigned char> pos_ctrl_ids;
+	std::vector<unsigned char> pos_ctrl_ids(pos_ctrl_ids_int.begin(), pos_ctrl_ids_int.end());
 	// ids of actuators controlled in speed (wheel mode)
-	std::vector<unsigned char> speed_ctrl_ids;
+	std::vector<unsigned char> speed_ctrl_ids(speed_ctrl_ids_int.begin(), speed_ctrl_ids_int.end());
 
 	std::vector<int> pos;
 
@@ -62,6 +76,36 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	bool missing_id = false;
+	for (unsigned char current_id : pos_ctrl_ids)
+	{
+		if (std::find(ids.begin(),ids.end(),current_id) == ids.end())
+		{
+			ROS_ERROR_STREAM("Dynamixel of id " << (int)current_id << " (joint mode) not found on the robot");
+			missing_id = true;
+		}
+	}
+	for (unsigned char current_id : speed_ctrl_ids)
+	{
+		if (std::find(ids.begin(),ids.end(),current_id) == ids.end())
+		{
+			ROS_ERROR_STREAM("Dynamixel of id " << (int)current_id << " (wheel mode) not found on the robot");
+			missing_id = true;
+		}
+	}
+	// exit the program if one or more actuator was not found
+	if (missing_id)
+		return 1;
+
+	// Show the lists of actuators we are controling
+	std::stringstream list_of_ids;
+	for(int i = 0; i < speed_ctrl_ids.size(); i++)
+		list_of_ids << (int)speed_ctrl_ids[i] << " ";
+	ROS_INFO_STREAM("Speed controlled ids : " << list_of_ids.str());
+	for(int i = 0; i < pos_ctrl_ids.size(); i++)
+		list_of_ids << (int)pos_ctrl_ids[i] << " ";
+	ROS_INFO_STREAM("Position controlled ids : " << list_of_ids.str());
+
 	// Client to get the current angular positions of each actuator
 	ros::ServiceClient pos_client = n.serviceClient<dynamixel_control::GetActuatorsPositions>("/dynamixel_control/getpositions");
 	dynamixel_control::GetActuatorsPositions pos_srv;
@@ -75,57 +119,16 @@ int main(int argc, char **argv)
 		ROS_ERROR("Failed to call service dynamixel_control/getpositions");
 		return -1;
 	}
+	// FIXME: do we even use the positions we get here ?
 
-	// FIXME: use configuration file instead
-	for(int j = 0; j < 3; j++) // originally j < 5
-	{
-		for(int i = 0; i < 6; i++)
-		{
-			if(std::find(ids.begin(),ids.end(),j*10+i+1) == ids.end())
-			{
-				ROS_ERROR("Dynamixel of id %d is not found",j*10+i+1);
-				return 1;
-			}
-			else
-			{
-				if(j < 4)
-					pos_ctrl_ids.push_back(j*10+i+1);
-				else if(j == 4)
-				{
-					speed_ctrl_ids.push_back(j*10+i+1);
-				}
-			}
-		}
-	}
+	std::vector<int> reference_pose;
+	n.getParam("reference_pose", reference_pose);
 
-	std::stringstream list_of_ids;
-	for(int i = 0; i < speed_ctrl_ids.size(); i++)
-		list_of_ids << (int)speed_ctrl_ids[i] << " ";
-	ROS_INFO_STREAM("Speed controlled ids : " << list_of_ids.str());
-
-	for(int i = 0; i < pos_ctrl_ids.size(); i++)
-		list_of_ids << (int)pos_ctrl_ids[i] << " ";
-	ROS_INFO_STREAM("Position controlled ids : " << list_of_ids.str());
-
-	pos[0] = 1498; // id 1
-	pos[1] = 2048; // id 2
-	pos[2] = 2598; // id 3
-	pos[3] = 1498; // id 4
-	pos[4] = 2048; // id 5
-	pos[5] = 2598; // id 6
-
-	int initial_offset = -512;//500;
-	for(int i = 0; i < 6; i++)
-	{
-		// pos[6+i] = 2048 + (i < 3 ? initial_offset : -initial_offset); // id 11 to 16
-		// pos[12+i] = 2048 + (i < 3 ? initial_offset : -initial_offset); // id 21 to 26
-		// pos[18+i] = 512; // id 31 to 36
-		// For Pexod
-		pos[6+i] = 2048 + initial_offset; // id 11 to 16
-		pos[12+i] = 2048 - initial_offset; // id 21 to 26
-		pos[18+i] = 512; // id 31 to 36
-	}
 	pos.resize(pos_ctrl_ids.size());
+	for (unsigned char id=0; id<pos_ctrl_ids.size(); id++)
+	{
+		pos[id] = reference_pose[id];
+	}
 
 	// Lower motors speeds for initial positionning
 	speed_msg.ids = pos_ctrl_ids;
@@ -171,35 +174,26 @@ int main(int argc, char **argv)
 		// oscillate around the initial position
 		int sin0 = (int)(amp0*sin(pulsation*elapsed));
 		int sin1 = (int)(amp1*cos(pulsation*elapsed));
-		int sin3 = (int)(amp3*sin(pulsation*elapsed));
 
-		pos_msg.positions[0] = pos[0] + sin0;
-		pos_msg.positions[1] = pos[1] - sin0;
-		pos_msg.positions[2] = pos[2] + sin0;
-		pos_msg.positions[3] = pos[3] + sin0;
-		pos_msg.positions[4] = pos[4] - sin0;
-		pos_msg.positions[5] = pos[5] + sin0;
-
-		pos_msg.positions[6] = pos[6] + sin1; // 11
-		pos_msg.positions[7] = pos[7] - sin1; // 12
-		pos_msg.positions[8] = pos[8] + sin1; // 13
-		pos_msg.positions[9] = pos[9] - sin1; // 14
-		pos_msg.positions[10] = pos[10] + sin1; // 15
-		pos_msg.positions[11] = pos[11] - sin1; // 16
-
-		pos_msg.positions[12] = pos[12] + sin1; // 21
-		pos_msg.positions[13] = pos[13] - sin1; // 22
-		pos_msg.positions[14] = pos[14] + sin1; // 23
-		pos_msg.positions[15] = pos[15] - sin1; // 24
-		pos_msg.positions[16] = pos[16] + sin1; // 25
-		pos_msg.positions[17] = pos[17] - sin1; // 26
-
-		pos_msg.positions[18] = pos[18] - sin3;
-		pos_msg.positions[19] = pos[19] + sin3;
-		pos_msg.positions[20] = pos[20] - sin3;
-		pos_msg.positions[21] = pos[21] - sin3;
-		pos_msg.positions[22] = pos[22] + sin3;
-		pos_msg.positions[23] = pos[23] - sin3;
+		for (unsigned char id=0; id<pos_ctrl_ids.size(); id++)
+		{
+			int increment;
+			switch (associated_oscillator[id])
+			{
+				case 0:
+					increment = sin0;
+					break;
+				case 1:
+					increment = sin1;
+					break;
+				default:
+					ROS_ERROR_STREAM("Actuator " << (int)id <<
+						" asks for unexisting oscillator " << associated_oscillator[id]);
+					increment = 0;
+			}
+			increment = (invert_position[id] ? -increment : increment);
+			pos_msg.positions[id] = pos[id] + increment;
+		}
 
 		position_pub.publish(pos_msg);
 
